55e6a06a587c90a3e6e879eb87635248b6c0665c
Roy Hyunjin Han <rhh2109@columbia.edu>
2016-09-23 16:27:00 -0400

    Work on merging with master

--- demo_sequencer.py
+++ demo_sequencer.py
@@ -1,9 +1,11 @@
+from os import makedirs
+from os.path import join
+
 from sequencer import NetworkPlan
 from sequencer.Models import EnergyMaximizeReturn
 
-from os.path import join
-source_folder = 'data/sumaila/input'
 
+source_folder = 'data/sumaila/input'
 csv = join(source_folder, 'metrics-local.csv')
 shp = join(source_folder, 'networks-proposed.shp')
 
@@ -11,12 +13,9 @@ nwp = NetworkPlan(shp, csv, prioritize='Population')
 model = EnergyMaximizeReturn(nwp)
 
 results = model.sequence()
-target_folder = 'xyz'
-
-from os import makedirs
+target_folder = '/tmp/sequencer'
 try:
     makedirs(target_folder)
 except OSError:
     pass
-
 model.output(target_folder)
