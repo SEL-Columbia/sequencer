e8f38fdb3a6dea6d3cdcea4943b71a4d75fdd08f
chrisnatali <chris.natali@gmail.com>
2017-09-17 19:06:20 -0400

    rel 0.3.2

--- sequencer/__init__.py
+++ sequencer/__init__.py
@@ -2,7 +2,7 @@ import logging
 from .NetworkPlan import NetworkPlan
 from .Sequencer import Sequencer
  
-__version__ = "0.3.1"
+__version__ = "0.3.2"
 
 logger = logging.getLogger('sequencer')
 logger.setLevel(logging.INFO)
